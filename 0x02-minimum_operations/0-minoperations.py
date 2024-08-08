9150282926361a9bfdb976007015e91cf8c2b6dd
taissir-l <128995791+taissir-l@users.noreply.github.com>
2024-08-08 16:55:31 +0100

    task finished

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/python3
+"""0-minoperations"""
+
+
+def minOperations(n):
+    """
+    minOperations
+    Gets the operations needed to result in exactly nH characters
+    """
+    if (n < 2):
+        return 0
+    ops, root = 0, 2
+    while root <= n:
+        if n % root == 0:
+            ops += root
+            n = n / root
+            root -= 1
+        root += 1
+    return ops
