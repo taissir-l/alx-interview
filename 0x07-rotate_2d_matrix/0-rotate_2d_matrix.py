5aad52f575d7e733d18559b8aa92434dc4d89fb0
taissir-l <128995791+taissir-l@users.noreply.github.com>
2024-09-12 20:53:53 +0100

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""
-to rotate 2D Matrix
-"""
+''' 2D matrix rotation clockwise '''
 
 
 def rotate_2d_matrix(matrix):
-    """rotate 2D matrix 90 degrees
-    Args:
-        matrix (list[[list]]): matrix
-    """
-    n = len(matrix)
-    for i in range(int(n / 2)):
-        y = (n - i - 1)
-        for j in range(i, y):
-            x = (n - 1 - j)
-            tmp = matrix[i][j]
-            matrix[i][j] = matrix[x][i]
-            matrix[x][i] = matrix[y][x]
-            matrix[y][x] = matrix[j][y]
-            matrix[j][y] = tmp
+    '''
+        rotates 2d matrix 90 degrees clockwise
+        args:
+            matrix
+    '''
+    rows = len(matrix)
+    columns = rows - 1
+
+    for i in range(0, int(rows / 2)):
+        for j in range(i, columns - i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[columns - j][i]
+            matrix[columns - j][i] = matrix[columns - i][columns - j]
+            matrix[columns - i][columns - j] = matrix[j][columns - i]
+            matrix[j][columns - i] = temp
